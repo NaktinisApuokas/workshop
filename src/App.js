3193a8265dc4b7bee5b049b7dc0824a2c85a8957
NaktinisApuokas <airidas.setkus@ktu.edu>
2023-02-01 19:18:45 +0200

    upda
    upda

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import getAge from "./helpers/getAge";
 function App() {
   const [date, setDate] = useState();
   const [age, setAge] = useState();
+  const [month, setMonth] = useState();
 
+  if (process.env.NODE_ENV !== "production")
+    return (
+      <div>
+        <h4>Date:</h4>
+        <input type="date" value={date} onChange={e => setDate(e.target.value)} ></input>
+
+        <h4>Age:</h4>
+        <input type="text" disabled value={age}></input>
+        <button onClick={() => setAge(getAge(date))}>Calculate</button>
+      </div>
+    );
   return (
     <div>
       <h4>Date:</h4>
       <input type="date" value={date} onChange={e => setDate(e.target.value)} ></input>
 
       <h4>Age:</h4>
+      <h4>Year:</h4>
       <input type="text" disabled value={age}></input>
-      <button onClick={() => setAge(getAge(date))}>Calculate</button>
-    </div>
+      <h4>Month:</h4>
+      <input type="text" disabled value={month}></input>
+      <button onClick={() => { setAge(getAge(date).age); setMonth(getAge(date).m) }}>Calculate</button>
+    </div >
   );
 }
 
--- src/helpers/getAge.js
+++ src/helpers/getAge.js
@@ -6,5 +6,5 @@ export default function getAge(dateString) {
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
-    return age;
+    return {age, m};
   }
